2a2f181fd49d33dab93ed261d247be3cf559680a
ktan126 <>
2021-06-06 20:30:49 +0000

    candidate-testing.js fixed error

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,11 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["1) Who was the first American woman in space? ",
-"2) True or false: 5 kilometer == 5000 meters? ",
-"3) (5 + 3) / 2 * 10 = ? ",
-"4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-"5) What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3) / 2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -27,7 +27,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   // candidateAnswer = input.question(question);
 for (let i = 0; i < questions.length; i++) {
-    candidateAnswer = input.question(questions[i]);
+    candidateAnswer = input.question(`${i+1}) ${questions[i]}`);
     candidateAnswers.push(candidateAnswer);
     console.log("Your answer: ", candidateAnswer);
     console.log("Correct answer: ", correctAnswers[i]);
@@ -61,7 +61,7 @@ for (let i = 0; i < questions.length; i++) {
     } else {
       console.log(">>> Status: FAILED <<<");
       } 
-     return grade;
+     return percent;
 }
 
 function runProgram() {
